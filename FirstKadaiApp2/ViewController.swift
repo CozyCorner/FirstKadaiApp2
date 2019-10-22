a332f51e593d71b4a2a28bdb35e32646fb92615b
cozy <14971352+CozyCorner@users.noreply.github.com>
2019-10-22 19:38:33 +0900

    Initial Commit

---
+++ FirstKadaiApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp2
+//
+//  Created by koji on 2019/10/22.
+//  Copyright © 2019 cozy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
